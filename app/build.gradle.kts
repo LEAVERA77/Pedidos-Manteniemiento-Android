e0b6b0cdcee375cea247b2f2246b51e8ce3e5710
LEAVERA77 <leavera77@gmail.com>
2026-05-09 23:27:18 -0300

    nueva version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.gestornova.gestion"
         minSdk = 24
         targetSdk = 35  // Usar 35 para compatibilidad
-        versionCode = 36
-        versionName = "1.0.36"
+        versionCode = 39
+        versionName = "1.0.39"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
